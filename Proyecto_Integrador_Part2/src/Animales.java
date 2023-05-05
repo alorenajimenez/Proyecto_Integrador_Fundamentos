7a623bc3606d905fc3e31f095846dc5ae95adeee
alorenajimenez <78825039+alorenajimenez@users.noreply.github.com>
2023-05-05 17:40:19 -0500

    Proyecto_Integrador_Part2

---
+++ Proyecto_Integrador_Part2/src/Animales.java
@@ -0,0 +1,42 @@
+
+public class Animales {
+	
+	private String nombre;
+	private String genero;
+	private String raza;
+	
+	public Animales(String nombre, String genero, String raza) {
+		super();
+		this.nombre = nombre;
+		this.genero = genero;
+		this.raza = raza;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getRaza() {
+		return raza;
+	}
+
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+	
+	
+	
+
+}
---
+++ Proyecto_Integrador_Part2/src/Veterinaria.java
@@ -0,0 +1,36 @@
+
+public class Veterinaria {
+	
+	private String Nombre;
+	private String Direccion;
+	private String Telefono;
+	
+	public Veterinaria(String nombre, String direccion, String telefono) {
+		super();
+		Nombre = nombre;
+		Direccion = direccion;
+		Telefono = telefono;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getDireccion() {
+		return Direccion;
+	}
+	public void setDireccion(String direccion) {
+		Direccion = direccion;
+	}
+	public String getTelefono() {
+		return Telefono;
+	}
+	public void setTelefono(String telefono) {
+		Telefono = telefono;
+	}
+	
+	
+	
+
+}
